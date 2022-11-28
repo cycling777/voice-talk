a8551ce119f53d5938ce3ac78c1c4b6a7d5cf027
IPI-k-watanabe <k_watanabe@ipi.co.jp>
2022-11-28 16:22:01 +0900

    fix: cdk.json

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ from talking3.pipeline_stack import PipelineStack
 
 
 app = cdk.App()
+deploy_target="dev" #default deploy target
+try:
+    deploy_target=app.node.try_get_context("deploy-target")
+except AttributeError:
+    deploy_target="dev"
+
 PipelineStack(
     app, 
     "PipelineStack", 
     env=cdk.Environment(
-        account="958152586967",
-        region="us-east-1"
+        account=app.node.try_get_context(deploy_target)["ACCOUNT"],
+        region=app.node.try_get_context(deploy_target)["REGION"],
     )
 )
 
--- talking3/pipeline_stack.py
+++ talking3/pipeline_stack.py
@@ -9,15 +9,29 @@ class PipelineStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Get Environment variables
+        deploy_target="dev" #default deploy target
+        try:
+            deploy_target=self.node.try_get_context("deploy-target")
+        except AttributeError:
+            deploy_target="dev"
+
+        account=self.node.try_get_context(deploy_target)["ACCOUNT"]
+        region=self.node.try_get_context(deploy_target)["REGION"]
+        repo_string = self.node.try_get_context(deploy_target)["GITHUB_REPOSITORY"]
+        branch = self.node.try_get_context(deploy_target)["GITHUB_BRANCH"]
+        connection_arn = self.node.try_get_context(deploy_target)["CODESTAR_CONNECTION_ARN"]
+
+        # Make pipeline
         pipeline = CodePipeline(
             self, "Pipeline",
             pipeline_name="MyPipeline",
             synth=ShellStep(
                 "Synth",
                 input=CodePipelineSource.connection(
-                    "IPI-k-watanabe/talking3",
-                    "main",
-                    connection_arn="arn:aws:codestar-connections:us-east-1:958152586967:connection/c5c2b5da-4b6a-4c74-937f-6a73532c0b0a",),
+                    repo_string=repo_string,
+                    branch=branch,
+                    connection_arn=connection_arn),
                 commands=[
                     "npm install -g aws-cdk",
                     "python -m pip install -r requirements.txt",
@@ -30,8 +44,8 @@ class PipelineStack(cdk.Stack):
                 self,
                 "Prod",
                 env=cdk.Environment(
-                    account="958152586967",
-                    region="us-east-1",
+                    account=account,
+                    region=region,
                 )
             )
         )
